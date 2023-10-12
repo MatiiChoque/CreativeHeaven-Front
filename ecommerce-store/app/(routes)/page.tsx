6121bd8780f89d16cdda570771d2587796966cc9
MatiiChoque <choquematia@gmail.com>
2023-10-11 21:04:55 -0300

    update

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,15 +1,21 @@
 import getBillboards from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboards("49cd6070-9f83-4e12-bd11-07ba8101b80f");
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
